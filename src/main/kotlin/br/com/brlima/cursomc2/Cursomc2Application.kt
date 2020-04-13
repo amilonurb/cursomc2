c0508ee15c241a517bff4c8dff90ce16de57c9ff
Bruno <brunolima.18@outlook.com>
2020-04-13 00:46:50 -0300

    - Model, Service, Resource e Repository de Categoria - Inclusão de arquivos de propriedades e classe para profile "test"

--- src/main/kotlin/br/com/brlima/cursomc2/Cursomc2Application.kt
+++ src/main/kotlin/br/com/brlima/cursomc2/Cursomc2Application.kt
@@ -1,5 +1,6 @@
 package br.com.brlima.cursomc2
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
