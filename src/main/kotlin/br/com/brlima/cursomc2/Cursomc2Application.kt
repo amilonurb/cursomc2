5f1367b6d18f10d8a984173522e43389c21e7852
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2020-04-12 23:43:25 -0300

    Versão inicial

---
+++ src/main/kotlin/br/com/brlima/cursomc2/Cursomc2Application.kt
@@ -0,0 +1,11 @@
+package br.com.brlima.cursomc2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Cursomc2Application
+
+fun main(args: Array<String>) {
+    runApplication<Cursomc2Application>(*args)
+}
